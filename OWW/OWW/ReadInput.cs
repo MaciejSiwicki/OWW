f9906115385a7d0e2748252cc0e557ddce59132e
Maciej Siwicki <maciej.siwicki00@gmail.com>
2023-11-11 11:30:46 +0100

    add seq solution

---
+++ OWW/OWW/ReadInput.cs
@@ -0,0 +1,21 @@
+﻿namespace OWW
+{
+    internal class ReadInput
+    {
+        public static int[,] readInput(string textFile)
+        {
+            string[] lines = File.ReadAllLines(textFile);
+            int size = lines.GetLength(0);
+            int[,] graph = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    graph[i, j] = Int32.Parse(lines[i].Split()[j]);
+                }
+            }
+            return graph;
+        }
+    }
+}
---
+++ OWW/OWW/TSPseq.cs
@@ -0,0 +1,45 @@
+﻿namespace OWW
+{
+    internal class TSPseq
+    {
+        static List<int> shortestPath = new List<int>();
+        static int Tsp(int[,] graph, bool[] visited, int currentPos, int numberOfCities, int count, int cost, int shortestPathCost, List<int> currentPath)
+        {
+            if (count == numberOfCities && graph[currentPos, 0] > 0)
+            {
+                if(shortestPathCost > cost + graph[currentPos, 0]) 
+                {
+                    shortestPathCost = cost + graph[currentPos, 0];
+                    shortestPath =  new List<int>(currentPath);
+                    shortestPath.Add(0);
+                }
+                return shortestPathCost;
+            }
+            for(int i = 0; i < numberOfCities;i++)
+            {
+                if (visited[i] == false && graph[currentPos,i] > 0) 
+                {
+                    visited[i] = true;
+                    currentPath.Add(i);
+                    shortestPathCost = Tsp(graph, visited, i, numberOfCities, count + 1, cost + graph[currentPos, i], shortestPathCost, currentPath);
+                    visited[i] = false;
+                    currentPath.RemoveAt(currentPath.Count - 1);
+                }
+            }
+            return shortestPathCost; 
+        }
+        private static void Main(string[] args)
+        {
+            int[,] graph = ReadInput.readInput("../../../input.txt");
+            int numberOfCities = graph.GetLength(0);
+            bool[] visitedCities = new bool[numberOfCities];
+            visitedCities[0] = true;
+            int shortestPathCost = int.MaxValue;
+            List<int> currentPath = new List<int>() { 0 };
+
+            shortestPathCost = Tsp(graph, visitedCities, 0, numberOfCities, 1, 0, shortestPathCost, currentPath);
+            Console.WriteLine(shortestPathCost);
+            Console.WriteLine(string.Join(" -> ", shortestPath));
+        }
+    }
+}
