f4602e4373ae8adda4fe1f04500b51855cd1038d
MaciejSiwicki <146980386+MaciejSiwicki@users.noreply.github.com>
2023-11-29 19:41:55 +0100

    M.siw seq (#1)
    * add seq solution
    
    * add timer
    
    * change tsp class
    
    * change input format
    
    * add write to file method
    
    * add visualization and generating input
    
    ---------
    
    Co-authored-by: Maciej Siwicki <maciej.siwicki00@gmail.com>

---
+++ OWW/OWW/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWW
+{
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<(int, int)> coordinates = FileController.ReadInput("../../../input.txt");
+            int[,] graph = FileController.CalculateDistanceMatrix(coordinates);
+            int numberOfCities = graph.GetLength(0);
+            bool[] visitedCities = new bool[numberOfCities];
+            visitedCities[0] = true;
+            int shortestPathCost = int.MaxValue;
+            List<int> currentPath = new List<int>() { 0 };
+
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            var seqMethod = new TSPseq();
+            shortestPathCost = seqMethod.Tsp(graph, visitedCities, 0, numberOfCities, 1, 0, shortestPathCost, currentPath);
+            watch.Stop();
+
+            Console.WriteLine(shortestPathCost);
+            Console.WriteLine(string.Join(" -> ", seqMethod.shortestPath));
+            Console.WriteLine(watch.Elapsed.TotalSeconds);
+            FileController.WriteToFile(seqMethod.shortestPath, "../../../output.txt");
+        }
+    }
+}
---
+++ OWW/OWW/TSPseq.cs
@@ -0,0 +1,32 @@
+﻿namespace OWW
+{
+    internal class TSPseq
+    {
+        public List<int> shortestPath = new List<int>();
+        public int Tsp(int[,] graph, bool[] visited, int currentPos, int numberOfCities, int count, int cost, int shortestPathCost, List<int> currentPath)
+        {
+            if (count == numberOfCities && graph[currentPos, 0] > 0)
+            {
+                if(shortestPathCost > cost + graph[currentPos, 0]) 
+                {
+                    shortestPathCost = cost + graph[currentPos, 0];
+                    shortestPath =  new List<int>(currentPath);
+                    shortestPath.Add(0);
+                }
+                return shortestPathCost;
+            }
+            for(int i = 0; i < numberOfCities;i++)
+            {
+                if (visited[i] == false && graph[currentPos,i] > 0) 
+                {
+                    visited[i] = true;
+                    currentPath.Add(i);
+                    shortestPathCost = Tsp(graph, visited, i, numberOfCities, count + 1, cost + graph[currentPos, i], shortestPathCost, currentPath);
+                    visited[i] = false;
+                    currentPath.RemoveAt(currentPath.Count - 1);
+                }
+            }
+            return shortestPathCost; 
+        }
+    }
+}
